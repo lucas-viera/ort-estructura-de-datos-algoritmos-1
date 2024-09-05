379bd4be37d46f19548a13b6317b300887c584c3
Lucas Viera <lucasviera22@gmail.com>
2024-09-05 09:29:34 -0300

    avance ejercicios

--- 2024s2_Ejercicios1/EjerciciosComienzo.cpp
+++ 2024s2_Ejercicios1/EjerciciosComienzo.cpp
@@ -1,30 +1,121 @@
 #include "EjerciciosComienzo.h"
 
+// INICIO SECCIÓN FUNCIONES AUXILIARES
+
+//TODO: agregar PRE y POST
+int maxComDivisor(int a, int b) {
+	int num1 = max(a, b);
+	int num2 = min(a, b);
+	int res;
+	do {
+		res = num2;
+		num2 = num1 % num2;
+		num1 = res;
+
+	} while (num2 != 0);
+	return res;
+}
+
+/*
+PRE: Recibe un vector de enteros y la cantidad de elementos del mismo
+POST: Devuelve el vector con sus posiciones incializadas en 0
+*/
+int* inicializarListaEnteros(int* lista, int largo) {
+	for (int i = 0; i < largo; i++) {
+		lista[i] = 0;
+	}
+	return lista;
+}
+
+/*
+PRE: Recibe un vector de bool y la cantidad de elementos del mismo
+POST: Devuelve el vector con sus posiciones incializadas en false
+*/
+bool* inicializarListaBool(bool* lista, int largo) {
+	for (int i = 0; i < largo; i++) {
+		lista[i] = false;
+	}
+	return lista;
+}
+
+// FIN SECCIÓN FUNCIONES AUXILIARES
+
 int suma(int a, int b) {
- 	// IMPLEMENTAR SOLUCION
-    return 0;
+    return a + b;
 }
 
 void tablaDel(unsigned int tablaDel, unsigned int desde, unsigned int hasta) {
- 	// IMPLEMENTAR SOLUCION
+	for (int i = desde; i <= hasta; i++) {
+		cout << i << "*" << tablaDel << "=" << i * tablaDel;
+		if (i != hasta) {
+			cout << ";";
+		}
+	}
 }
+
+
 
 void simplificar(int n, int d) {
-	// IMPLEMENTAR SOLUCION
+	if (n != 0) {
+		int mcd = maxComDivisor(n, d);
+		n = n / mcd;
+		d = d / mcd;
+	}
+	if (d < 0) {
+		n = -n;
+		d = -d;
+	}
+	
+	cout << n << "/" << d;
 }
 
+
 int ocurrencias123Repetidos(int* vector, int largo) {
-	// IMPLEMENTAR SOLUCION
-	return 0;
+	int contador = 0;
+	bool* flags = new bool[4];
+	flags = inicializarListaBool(flags, 4);
+
+	for (int i = 0; i < largo; i++) {
+		if (vector[i] == 1 && !flags[2]) {
+			flags[1] = true;
+		}
+		else if (vector[i] == 2 && flags[1] && !flags[3]) {
+			flags[2] = true;
+		}
+		else if (vector[i] == 3 && flags [2]) {
+			flags[3] = true;
+		}
+		else {
+			flags = inicializarListaBool(flags, 4);
+		}
+
+		if (flags[1] && flags[2] && flags[3]) {
+			contador++;
+			flags = inicializarListaBool(flags, 4);
+		}
+	}
+	return contador;
 }
 
 int maximoNumero(unsigned int n) {
-	// IMPLEMENTAR SOLUCION
-    return 0;
+	int maximo = INT_MIN;
+	int* numeros = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> numeros[i];
+	}
+	for (int i = 0; i < n; i++) {
+		if (maximo < numeros[i]) {
+			maximo = numeros[i];
+		}
+	}
+    return maximo;
 }
 
 void ordenarVecInt(int *vec, int largoVec) {
-	// IMPLEMENTAR SOLUCION
+	
+	
+
+	
 }
 
 int* intercalarVector(int* v1, int* v2, int l1, int l2){
@@ -41,6 +132,7 @@ bool subconjuntoVector(int* v1, int* v2, int l1, int l2)
 char* invertirCase(char* str)
 {
 	// IMPLEMENTAR SOLUCION
+	//hacer un new, reservar memoria y retornar el char*
 	return NULL;
 }
 
