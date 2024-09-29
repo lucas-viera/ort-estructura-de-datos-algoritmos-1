647e2925d1d0501af4ac83f5ea14de3781f230a5
Lucas Viera <lucasviera22@gmail.com>
2024-09-29 20:09:39 -0300

    comienzo ejercicios listas

--- 2024s2_Ejercicios1/EjerciciosListas.cpp
+++ 2024s2_Ejercicios1/EjerciciosListas.cpp
@@ -1,9 +1,83 @@
 #include "EjerciciosListas.h"
 
-NodoLista* invertirParcial(NodoLista* l) 
-{
-	// IMPLEMENTAR SOLUCION
-	return NULL;
+// INICIO SECCIÓN FUNCIONES AUXILIARES
+
+/*
+PRE: recibe una lista y un entero
+POS: crea un nodo y apendea el nuevo nodo al comienzo de la lista. 
+	Modifica el puntero a la lista original
+*/
+void agregarAlInicio(NodoLista*& l, int x) {
+	NodoLista* nuevoNodo = new NodoLista;
+	nuevoNodo->dato = x;
+	nuevoNodo->sig = l;
+	l = nuevoNodo;
+}
+
+/*
+PRE:
+POS:
+*/
+int cantNodos(NodoLista* l) {
+	int cant = 0;
+	while (l->sig != NULL) {
+		cant++;
+		l = l->sig;
+	}
+	return cant+1;
+}
+
+/*
+PRE:
+POS:
+*/
+NodoLista* copiarLista(NodoLista* l) {
+	if (l == NULL){
+		return NULL;
+	}
+	// primer nodo de la lista copiada
+	NodoLista* nuevaLista = new NodoLista;
+	nuevaLista->dato = l->dato;
+	nuevaLista->sig = NULL;			//puede ser innecesario
+
+	// Cursor para recorrer l original y la lista copiada
+	NodoLista* cursor = l->sig;
+	NodoLista* auxNueva = nuevaLista;
+
+	while (cursor != NULL) {
+		//nodo nuevo par anexar a la copia
+		NodoLista* nodoAux = new NodoLista;
+		nodoAux->dato = cursor->dato;
+		nodoAux->sig = NULL;
+
+		auxNueva->sig = nodoAux;	//anexo nodo nuevo
+		//avanzar 
+		auxNueva = nodoAux;			//lista aux apunta a nodo nuevo
+		cursor = cursor->sig;		//cursor avanza
+	}
+
+	return nuevaLista;
+}
+
+// FIN SECCIÓN FUNCIONES AUXILIARES
+
+
+
+NodoLista* invertirParcial(NodoLista* l) {
+	if (l == NULL || l->sig == NULL) {
+		return NULL;
+	}
+
+	NodoLista* nuevaLista = NULL;
+	NodoLista* cursor = l;
+
+	while (cursor->sig->sig != NULL) {		// Hasta el penúltimo elemento
+		agregarAlInicio(nuevaLista, cursor->dato);
+		cursor = cursor->sig;
+	}
+
+	agregarAlInicio(nuevaLista, cursor->dato); // Penúltimo elemento agregado
+	return nuevaLista;
 }
 
 void eliminarNesimoDesdeElFinal(NodoLista*& lista, int &n) 
